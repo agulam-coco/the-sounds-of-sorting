30f20d4396851c3c7ac2ad8c754c42033b0fc41d
agulam-coco <aguchija@gmail.com>
2025-03-19 15:42:55 -0500

    implemented insertion, selection, bubble and merge sort

--- src/test/java/edu/grinnell/csc207/soundsofsorting/SortsTests.java
+++ src/test/java/edu/grinnell/csc207/soundsofsorting/SortsTests.java
@@ -7,14 +7,17 @@ import java.util.function.Consumer;
 import org.junit.jupiter.api.Test;
 
 import edu.grinnell.csc207.soundsofsorting.sorts.Sorts;
+import java.util.Arrays;
 
 public class SortsTests {
+
     /**
      * @param <T> the carrier type of the array
      * @param arr the array to check
-     * @return true iff <code>arr</code> is sorted.
+     * @return true if <code>arr</code> is sorted.
      */
     public static <T extends Comparable<? super T>> boolean sorted(T[] arr) {
+
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i].compareTo(arr[i + 1]) > 0) {
                 return false;
@@ -24,7 +27,10 @@ public class SortsTests {
     }
 
     public static Integer[] makeTestArray() {
-        return new Integer[] {
+//        return new Integer[]{
+//            2,1,4,5,4,5,6,9,4,3
+//        };
+        return new Integer[]{
             3, 7, 9, 1, 2,
             18, 16, 15, 19, 8,
             14, 12, 5, 13, 4,
@@ -35,6 +41,7 @@ public class SortsTests {
     public void testSort(Consumer<Integer[]> func) {
         Integer[] arr = makeTestArray();
         func.accept(arr);
+        System.out.println(Arrays.toString(arr));
         assertTrue(sorted(arr));
     }
 
@@ -42,12 +49,12 @@ public class SortsTests {
     public void testBubbleSort() {
         testSort(Sorts::bubbleSort);
     }
-    
+
     @Test
     public void testInsertionSort() {
         testSort(Sorts::insertionSort);
     }
-    
+
     @Test
     public void testSelectionSort() {
         testSort(Sorts::selectionSort);
@@ -57,7 +64,7 @@ public class SortsTests {
     public void testMergeSort() {
         testSort(Sorts::mergeSort);
     }
-    
+
     @Test
     public void testQuickSort() {
         testSort(Sorts::quickSort);
