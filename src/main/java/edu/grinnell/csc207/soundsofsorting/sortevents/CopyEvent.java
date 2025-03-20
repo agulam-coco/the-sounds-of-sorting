b0db550a4c721c8e409a2f3de36f6dbcb808b8e9
agulam-coco <aguchija@gmail.com>
2025-03-20 15:40:48 -0500

    added sorting event functionality to insertion ,selection, quicksort and part of tim sort

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
@@ -1,8 +1,40 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A <code>CopyEvent</code> logs a copy of a value into an index of the array.
  */
-public class CopyEvent<T> {
-    // TODO: implement me!
+public class CopyEvent<T> implements SortEvent<T>{
+   
+    private List<Integer> indeces;
+    
+    /**
+     * Initialize Compare Event and add compared events to a list
+     * @param firstIndex
+     * @param secondIndex 
+     */
+    public CopyEvent(int firstIndex, int secondIndex){
+        indeces = new ArrayList<>();
+        indeces.add(firstIndex);
+        indeces.add(secondIndex);
+    }
+    
+    //Does nothing to the array
+    @Override
+    public void apply(T[] arr) {
+        arr[getAffectedIndices().get(0)] = arr[getAffectedIndices().get(1)];
+    }
+    
+    @Override
+     public List<Integer> getAffectedIndices(){
+         return indeces;
+     }
+    
+    @Override
+    public boolean isEmphasized(){
+        //is emphasized
+        return true;
+    }
 }
